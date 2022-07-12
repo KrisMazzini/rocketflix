46592534da04d0895907b320d3ada9d075e745b4
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-07-11 22:19:01 -0300

    include form button and page footer

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,11 +2,22 @@ import './styles.css'
 
 export function Home() {
     return (
-        <>
+        <main>
             <header>
                 <img src="/shuffle.svg" alt="Icon"/>
                 <h1>Não sabe o que assistir?</h1>
             </header>
-        </>
+            <form action="" method="get">
+                <button type="submit">
+                    <img src="/shuffle.svg" alt="Icon" />
+                    <p>Encontrar filme</p>
+                </button>
+            </form>
+            <footer>
+                <p>
+                    Clique em "Encontrar filme" que traremos informações de algum filme para você assistir hoje.
+                </p>
+            </footer>
+        </main>
     )
 }
