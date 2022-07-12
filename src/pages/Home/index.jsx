aba15c40b10f3030adca30821291302cfdd004d9
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-07-11 21:11:19 -0300

    include header element

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,12 @@
+import './styles.css'
+
 export function Home() {
     return (
-        <></>
+        <>
+            <header>
+                <img src="/shuffle.svg" alt="Icon"/>
+                <h1>Não sabe o que assistir?</h1>
+            </header>
+        </>
     )
 }
