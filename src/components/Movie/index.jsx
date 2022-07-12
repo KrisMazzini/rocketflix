3ce0c7b88b13becb70bd000d54547c9744c155c8
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-07-12 16:22:15 -0300

    use The Movie Database to get random movies

--- src/components/Movie/index.jsx
+++ src/components/Movie/index.jsx
@@ -1,12 +1,12 @@
 import './styles.css'
 
-export function Movie() {
+export function Movie({title, overview, poster}) {
     return (
         <div className='movie'>
-            <img src="https://picsum.photos/171/244" alt="Movie cover" />
+            <img src={poster} alt="Movie cover" />
             <div className="movie-details">
-                <h2>Movie title</h2>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore explicabo quidem neque architecto molestias? Harum architecto nisi illo impedit, deleniti quod sed voluptatem incidunt laborum, laboriosam, maiores minima quis eaque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda commodi voluptate perspiciatis earum totam officia sit dolorem ea esse, maiores in deleniti molestias recusandae minima harum nemo dolor expedita architecto!</p>
+                <h2>{title}</h2>
+                <p>{overview}</p>
             </div>
         </div>
     )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,71 @@
-import './styles.css'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 import { Movie } from '../../components/Movie'
+import { API_KEY, BASE_URL, IMG_URL } from '../../services/api'
+
+import './styles.css'
 
 export function Home() {
+
+    const [movie, setMovie] = useState()
+
+    async function handleShuffleMovie() {
+        try {
+            const page = getRandomInteger(1, 500)
+            const api = `${BASE_URL}?api_key=${API_KEY}&page=${page}`
+            const response = await axios.get(api)
+            const popularMovies = response.data.results
+            const chosenMovie = getRandomMovie(popularMovies)
+
+            setMovie({
+                title: chosenMovie.title,
+                overview: chosenMovie.overview,
+                poster: `${IMG_URL}/${chosenMovie.poster_path}`
+            })
+        } catch(err) {
+            console.error(`There was an error:\n\n${err}`)
+            setMovie({
+                title: "Oops, it looks like today is not a good day for movies... Let's code! 🚀",
+                overview: "",
+                poster: "/Poster.png"
+            })
+        } finally {
+            console.log("Request concluded.")
+        }
+    }
+
+    function getRandomMovie(movies) {
+        const minIndex = 0
+        const maxIndex = movies.length - 1
+        const randomIndex = getRandomInteger(minIndex, maxIndex)
+
+        return movies[randomIndex]
+    }
+
+    function getRandomInteger(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
     return (
         <main>
             <header>
                 <img src="/shuffle.svg" alt="Icon"/>
                 <h1>Não sabe o que assistir?</h1>
             </header>
-            <Movie />
-            <form action="" method="get">
-                <button type="submit">
+            {
+                !!movie ? (
+                    <Movie 
+                        title={movie.title}
+                        overview={movie.overview}
+                        poster={movie.poster}
+                    />
+                ) : <></>
+            }
+            <form action="">
+                <button type="button" onClick={handleShuffleMovie}>
                     <img src="/shuffle.svg" alt="Icon" />
                     <p>Encontrar filme</p>
                 </button>
