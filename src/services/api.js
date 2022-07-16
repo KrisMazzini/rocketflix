683962f2150a6af723a0ffdd8034c0715f2dd764
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-07-15 21:39:54 -0300

    remove api.js

--- src/services/api.js
+++
@@ -1,3 +0,0 @@
-export const API_KEY = '19363e26b0c5e56c72b47e3933deb1b0'
-export const BASE_URL = 'https://api.themoviedb.org/3/movie/popular'
-export const IMG_URL = 'https://image.tmdb.org/t/p/w500'
